8c39a4753d9a7bf8c2e664dfca51c3ba5293a2b8
Xiaowei <145980509+Xiaowei6636@users.noreply.github.com>
2025-12-21 21:27:43 +0800

    Modify hotel notes in data.js
    Updated hotel notes to be empty strings except for one.

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 export const hotels = [
-    { name: "V Hotel Bencoolen", price: 6377, cancelable: false, size: 16, note: "16平方公尺" },
-    { name: "Hotel Mi Bencoolen", price: 6507, cancelable: true, size: 17, note: "17平方公尺" },
-    { name: "Hotel Mi Rochor", price: 6627, cancelable: false, size: 17, note: "17平方公尺" },
-    { name: "Furama RiverFront", price: 5360, cancelable: false, size: 30, note: "30平方公尺" },
-    { name: "Owen House by Habyt", price: 8271, cancelable: false, size: 32, note: "32平方公尺" },
-    { name: "V Hotel Lavender", price: 6883, cancelable: false, size: 16, note: "16平方公尺" },
-    { name: "Hotel Classic by Venue", price: 4300, cancelable: true, size: 18, note: "18平方公尺/部分無窗" }
+    { name: "V Hotel Bencoolen", price: 6377, cancelable: false, size: 16, note: "" },
+    { name: "Hotel Mi Bencoolen", price: 6507, cancelable: true, size: 17, note: "" },
+    { name: "Hotel Mi Rochor", price: 6627, cancelable: false, size: 17, note: "" },
+    { name: "Furama RiverFront", price: 5360, cancelable: false, size: 30, note: "" },
+    { name: "Owen House by Habyt", price: 8271, cancelable: false, size: 32, note: "" },
+    { name: "V Hotel Lavender", price: 6883, cancelable: false, size: 16, note: "" },
+    { name: "Hotel Classic by Venue", price: 4300, cancelable: true, size: 18, note: "部分無窗" }
 ];
