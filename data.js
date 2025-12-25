0eba6cf3c22ddae254220dfb43d3295d23e95f97
Xiaowei <145980509+Xiaowei6636@users.noreply.github.com>
2025-12-25 15:40:29 +0800

    Update Strand Hotel Singapore details in data.js

--- data.js
+++ data.js
@@ -6,7 +6,9 @@ export const hotels = [
     { name: "Owen House by Habyt", price: 8271, cancelable: false, size: 32, note: "" },
     { name: "V Hotel Lavender", price: 6883, cancelable: false, size: 16, note: "" },
     { name: "Hotel Classic by Venue", price: 4300, cancelable: true, size: 18, note: "部分無窗" },
-    { name: "Strand Hotel Singapore", price: 6500, cancelable: true, size: 34, hasSofaBed: false, hasFewOutlets: false, hasNoParking: false, note: "免費無限網路/入住前2天並在14點前方可免費取消" }
+    { name: "Strand Hotel Singapore", price: 6500, cancelable: true, size: 18, hasFewOutlets: true, note: "三人房(18/2sig+1ex)/WiFI/前2天14點前方可取消" },
+    { name: "Strand Hotel Singapore", price: 6800, cancelable: true, size: 20, hasFewOutlets: false, note: "三人房(20/2sig+1ex)/WiFI/前2天14點前方可取消" },
+    { name: "Strand Hotel Singapore", price: 8850, cancelable: true, size: 34, hasFewOutlets: false, note: "家庭房(2張雙人床/WiFi/前2天14點前方可取消" }
 ];
 
 
