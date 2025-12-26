67200a5c9110251779220488d33305d1c258add5
Xiaowei <145980509+Xiaowei6636@users.noreply.github.com>
2025-12-26 23:47:31 +0800

    Enhance V Hotel Bencoolen details
    Updated hotel description for V Hotel Bencoolen to include additional details about the room and amenities.
    Data from google maps.

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 export const hotels = [
-    { name: "V Hotel Bencoolen", price: 6377, cancelable: false, size: 16, note: "", hasWiFi: true, hasPool: true, isRedLightDistrict: false },
+    { name: "V Hotel Bencoolen", price: 6377, cancelable: false, size: 16, note: "房間大多整節乾淨但擁擠/服務好/飯店內有迷你超市(未證實)/號稱出站7秒可達/無瓶裝水/部份房間有異味或窗戶正對泳池/健身房", hasWiFi: true, hasPool: true, isRedLightDistrict: false, hasWashingMachine: true, hasFewOutlets: true },
     { name: "Hotel Mi Bencoolen", price: 6507, cancelable: true, size: 17, note: "", hasWiFi: true, isRedLightDistrict: false },
     { name: "Hotel Mi Rochor", price: 6627, cancelable: false, size: 17, note: "", hasWiFi: true, hasPool: true, isRedLightDistrict: false },
     { name: "Furama RiverFront", price: 6702, cancelable: false, size: 30, note: "健身房/酒吧/水療服務/無客房清潔", hasWiFi: true, hasPool: true, hasWashingMachine: true, isRedLightDistrict: false },
