290d98522033fc4f3d545e99147692d828123e80
Xiaowei <145980509+Xiaowei6636@users.noreply.github.com>
2025-12-27 17:58:17 +0800

    Add new hotel: Test Hotle Taiwan

--- data.js
+++ data.js
@@ -1,14 +1,120 @@
 export const hotels = [
-    { name: "V Hotel Bencoolen", price: 6377, cancelable: false, size: 16, note: "房間大多整節乾淨但擁擠/服務好/飯店內有迷你超市(未證實)/號稱出站7秒可達/無瓶裝水/部份房間有異味或窗戶正對泳池/健身房", hasWiFi: true, hasPool: true, isRedLightDistrict: false, hasWashingMachine: true, hasFewOutlets: true, isPoorSoundproofing: true, hasSofaBed: false },
-    { name: "Hotel Mi Bencoolen", price: 6507, cancelable: true, size: 17, note: "", hasWiFi: true, isRedLightDistrict: false },
-    { name: "Hotel Mi Rochor", price: 6627, cancelable: false, size: 17, note: "", hasWiFi: true, hasPool: true, isRedLightDistrict: false },
-    { name: "Furama RiverFront", price: 6702, cancelable: false, size: 30, note: "健身房/酒吧/水療服務/無客房清潔", hasWiFi: true, hasPool: true, hasWashingMachine: true, isRedLightDistrict: false },
-    { name: "Owen House by Habyt", price: 8271, cancelable: false, size: 32, note: "機場接駁/酒吧/部分有廚房", hasWiFi: true, isRedLightDistrict: false },
-    { name: "V Hotel Lavender", price: 6883, cancelable: false, size: 16, note: "酒吧", hasWiFi: true, hasPool: true, isRedLightDistrict: false },
-    { name: "Hotel Classic by Venue", price: 4300, cancelable: true, size: 18, note: "部分無窗", hasWiFi: true, lat: 1.3159092188196442, lon: 103.8977450655825, isRedLightDistrict: true, isPoorSoundproofing: true },
-    { name: "Strand Hotel Singapore", price: 6500, cancelable: true, size: 18, hasFewOutlets: true, note: "三人房(18/2sig+1ex)/前2天14點前方可取消", hasWiFi: true, hasWashingMachine: true, isRedLightDistrict: false },
-    { name: "Strand Hotel Singapore", price: 6800, cancelable: true, size: 20, hasFewOutlets: false, note: "三人房(20/2sig+1ex)/前2天14點前方可取消", hasWiFi: true, hasWashingMachine: true, isRedLightDistrict: false },
-    { name: "Strand Hotel Singapore", price: 8850, cancelable: true, size: 34, hasFewOutlets: false, note: "家庭房(2張雙人床)/前2天14點前方可取消", hasWiFi: true, hasWashingMachine: true, isRedLightDistrict: false }
+    {
+        "name": "V Hotel Bencoolen",
+        "price": 6377,
+        "cancelable": false,
+        "size": 16,
+        "note": "房間大多整節乾淨但擁擠/服務好/飯店內有迷你超市(未證實)/號稱出站7秒可達/無瓶裝水/部份房間有異味或窗戶正對泳池/健身房",
+        "hasWiFi": true,
+        "hasPool": true,
+        "isRedLightDistrict": false,
+        "hasWashingMachine": true,
+        "hasFewOutlets": true,
+        "isPoorSoundproofing": true,
+        "hasSofaBed": false
+    },
+    {
+        "name": "Hotel Mi Bencoolen",
+        "price": 6507,
+        "cancelable": true,
+        "size": 17,
+        "note": "",
+        "hasWiFi": true,
+        "isRedLightDistrict": false
+    },
+    {
+        "name": "Hotel Mi Rochor",
+        "price": 6627,
+        "cancelable": false,
+        "size": 17,
+        "note": "",
+        "hasWiFi": true,
+        "hasPool": true,
+        "isRedLightDistrict": false
+    },
+    {
+        "name": "Furama RiverFront",
+        "price": 6702,
+        "cancelable": false,
+        "size": 30,
+        "note": "健身房/酒吧/水療服務/無客房清潔",
+        "hasWiFi": true,
+        "hasPool": true,
+        "hasWashingMachine": true,
+        "isRedLightDistrict": false
+    },
+    {
+        "name": "Owen House by Habyt",
+        "price": 8271,
+        "cancelable": false,
+        "size": 32,
+        "note": "機場接駁/酒吧/部分有廚房",
+        "hasWiFi": true,
+        "isRedLightDistrict": false
+    },
+    {
+        "name": "V Hotel Lavender",
+        "price": 6883,
+        "cancelable": false,
+        "size": 16,
+        "note": "酒吧",
+        "hasWiFi": true,
+        "hasPool": true,
+        "isRedLightDistrict": false
+    },
+    {
+        "name": "Hotel Classic by Venue",
+        "price": 4300,
+        "cancelable": true,
+        "size": 18,
+        "note": "部分無窗",
+        "hasWiFi": true,
+        "lat": 1.3159092188196442,
+        "lon": 103.8977450655825,
+        "isRedLightDistrict": true,
+        "isPoorSoundproofing": true
+    },
+    {
+        "name": "Strand Hotel Singapore",
+        "price": 6500,
+        "cancelable": true,
+        "size": 18,
+        "hasFewOutlets": true,
+        "note": "三人房(18/2sig+1ex)/前2天14點前方可取消",
+        "hasWiFi": true,
+        "hasWashingMachine": true,
+        "isRedLightDistrict": false
+    },
+    {
+        "name": "Strand Hotel Singapore",
+        "price": 6800,
+        "cancelable": true,
+        "size": 20,
+        "hasFewOutlets": false,
+        "note": "三人房(20/2sig+1ex)/前2天14點前方可取消",
+        "hasWiFi": true,
+        "hasWashingMachine": true,
+        "isRedLightDistrict": false
+    },
+    {
+        "name": "Strand Hotel Singapore",
+        "price": 8850,
+        "cancelable": true,
+        "size": 34,
+        "hasFewOutlets": false,
+        "note": "家庭房(2張雙人床)/前2天14點前方可取消",
+        "hasWiFi": true,
+        "hasWashingMachine": true,
+        "isRedLightDistrict": false
+    },
+    {
+        "name": "Test Hotle Taiwan",
+        "price": 1000000,
+        "size": 0,
+        "note": "BED IDEA",
+        "cancelable": false,
+        "isSafeLocation": false
+    }
 ];
 
 
